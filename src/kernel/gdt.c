8e3a01315c8cb055650175720867cb531076d0c2
simone222222 <simone.paolini14@gmail.com>
2026-01-06 14:56:58 +0100

    Implement Global Descriptor Table (GDT) initialization

---
+++ src/kernel/gdt.c
@@ -0,0 +1,31 @@
+/*
+ * Global Descriptor Table (GDT)
+ */
+
+#include "../include/gdt.h"
+
+gdt_entry_t gdt[3];
+gdt_ptr_t gdt_ptr;
+
+void gdt_init() {
+    gdt_ptr. limit = (sizeof(gdt_entry_t) * 3) - 1;
+    gdt_ptr.base = (uint32_t)&gdt;
+    
+    gdt_set_gate(0, 0, 0, 0, 0);      // Null segment
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
+    
+    gdt_flush((uint32_t)&gdt_ptr);
+}
+
+void gdt_set_gate(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
+    gdt[num].base_low = (base & 0xFFFF);
+    gdt[num].base_middle = (base >> 16) & 0xFF;
+    gdt[num].base_high = (base >> 24) & 0xFF;
+    
+    gdt[num].limit_low = (limit & 0xFFFF);
+    gdt[num].granularity = (limit >> 16) & 0x0F;
+    
+    gdt[num].granularity |= gran & 0xF0;
+    gdt[num].access = access;
+}
